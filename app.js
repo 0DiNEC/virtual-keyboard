0c334284bba3b9d21fa7d69f757d47cc9fd83bea
0DiNEC <as.odinson@gmail.com>
2023-04-30 13:45:26 +0300

    feat: add keycode array and mouse click event

--- app.js
+++ app.js
@@ -84,7 +84,72 @@ const englishKeyboardLayout = [
   ],
 ];
 
-const keyboardCode = [];
+const keyboardCode = [
+  "Backquote",
+  "Digit1",
+  "Digit2",
+  "Digit3",
+  "Digit4",
+  "Digit5",
+  "Digit6",
+  "Digit7",
+  "Digit8",
+  "Digit9",
+  "Digit0",
+  "Minus",
+  "Equal",
+  "Backspace",
+  "Tab",
+  "KeyQ",
+  "KeyW",
+  "KeyE",
+  "KeyR",
+  "KeyT",
+  "KeyY",
+  "KeyU",
+  "KeyI",
+  "KeyO",
+  "KeyP",
+  "BracketLeft",
+  "BracketRight",
+  "Backslash",
+  "Delete",
+  "CapsLock",
+  "KeyA",
+  "KeyS",
+  "KeyD",
+  "KeyF",
+  "KeyG",
+  "KeyH",
+  "KeyJ",
+  "KeyK",
+  "KeyL",
+  "Semicolon",
+  "Quote",
+  "Enter",
+  "ShiftLeft",
+  "KeyZ",
+  "KeyX",
+  "KeyC",
+  "KeyV",
+  "KeyB",
+  "KeyN",
+  "KeyM",
+  "Comma",
+  "Period",
+  "Slash",
+  "ArrowUp",
+  "ShiftRight",
+  "ControlLeft",
+  "MetaLeft",
+  "AltLeft",
+  "Space",
+  "AltRight",
+  "ArrowLeft",
+  "ArrowDown",
+  "ArrowRight",
+  "ControlRight",
+];
 
 function buildHTML() {
   document.body.className = "body";
@@ -96,15 +161,29 @@ function buildHTML() {
      </main>`
   );
 
+  // create keyboard
   let keyboard = document.querySelector(".keyboard");
   let keyButtonNum = 0;
   for (let i = 0; i < englishKeyboardLayout.length; i++) {
     let innerHTML = `<div class="keyboard__row __row${i}">`;
     for (let j = 0; j < englishKeyboardLayout[i].length; j++, keyButtonNum++) {
-      innerHTML += `<span class="key-button key-button${keyButtonNum}">${englishKeyboardLayout[i][j][0]} </span>`;
+      innerHTML += `<span class="key-button key-button${keyButtonNum}">${englishKeyboardLayout[i][j][0]}</span>`;
     }
     innerHTML += `</div>`;
     keyboard.insertAdjacentHTML("beforeend", innerHTML);
   }
 }
 buildHTML();
+
+// virtual keyboard mouse down
+let textarea = document.querySelector(".textarea");
+let keyButtons = document.querySelectorAll(".key-button");
+keyButtons.forEach((button) => {
+  button.addEventListener(
+    "mousedown",
+    () => (textarea.textContent += button.textContent),
+    false
+  );
+});
+
+function keyDown(button) {}
